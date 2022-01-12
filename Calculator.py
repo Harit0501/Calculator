098c0a6954d2d025ea27961eaa7419b55b9d070e
Harit0501 <97599086+Harit0501@users.noreply.github.com>
2022-01-12 16:03:49 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+print('Simple Calculator') #Use of print statement
+x = int(input("Enter the first number :")) #Integer data type
+y = int(input("Enter the second number :")) #Integer data type
+
+sum = x + y
+difference = x - y 
+product = x * y
+quotient = x / y
+remainder = x % y
+
+print("The sum of x and y is ", sum)
+print("The difference between x and y is ", difference)
+print("The product of x and y is ", product)
+print("The quotient of x and y is ", quotient)
+print("The remainder of x and y is ", remainder)
